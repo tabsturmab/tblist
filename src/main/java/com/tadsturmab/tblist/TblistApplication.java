d40f9fac4ca518e8d1490acf1bc22260fa330ada
Andre Silva <Andre Silva>
2024-01-05 14:34:23 -0300

    Project created

---
+++ src/main/java/com/tadsturmab/tblist/TblistApplication.java
@@ -0,0 +1,13 @@
+package com.tadsturmab.tblist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TblistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TblistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadsturmab/tblist/TblistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadsturmab.tblist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TblistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
