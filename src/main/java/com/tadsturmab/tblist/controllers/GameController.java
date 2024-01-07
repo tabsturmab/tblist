8ad451f4815af7e27dc5a381543afca7e2615995
Andre Silva <Andre Silva>
2024-01-07 08:29:23 -0300

    Get name by Id

--- src/main/java/com/tadsturmab/tblist/controllers/GameController.java
+++ src/main/java/com/tadsturmab/tblist/controllers/GameController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tadsturmab.tblist.dto.GameDTO;
 import com.tadsturmab.tblist.dto.GameMinDTO;
-import com.tadsturmab.tblist.entities.Game;
 import com.tadsturmab.tblist.services.GameService;
 
 @RestController
@@ -17,6 +18,12 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
